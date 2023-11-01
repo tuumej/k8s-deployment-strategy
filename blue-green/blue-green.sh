9aa49a968431f3301a01b4aecfbfa82bcba1f465
tuumej <tuumej@gmail.com>
2023-11-01 10:06:25 +0900

    k8s deploy strategy

---
+++ blue-green/blue-green.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#kubectl patch svc tomcat-svc -p '{"spec":{"selector":{"app":"tomcat-pod-green"}}}'
+
+#kubectl patch svc tomcat-svc -p '{"spec":{"selector":{"app":"tomcat-pod-blue"}}}'
+
---
+++ blue-green/tmp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+  <head></head>
+  <body bgcolor='#3581C1'>
+    This version is blue
+  </body>
+</html>
